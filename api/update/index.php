1ac319c0a5fb7ed02e2b188d5fd7241c90471bec
KEINOS <github@keinos.com>
2018-05-23 02:17:16 +0900

    feat: Force run via Web

--- api/update/index.php
+++ api/update/index.php
@@ -9,35 +9,46 @@ header("Content-Type: text/plain");
 
 $name_file_updater = 'clone_repo.php';
 $name_dir_parent   = '..';
-$path_dir_parent   = $name_dir_parent . DIR_SEP;
+$path_dir_root     = $_SERVER['DOCUMENT_ROOT'];
 
-// DocumentRoot/api/update/index.php
-$path_dir_root     = $path_dir_parent . $path_dir_parent . $path_dir_parent;
-$path_dir_root     = realpath(__DIR__ . DIR_SEP . $path_dir_root);
-$path_file_updater = $path_dir_root . DIR_SEP . $name_file_updater;
-
-if(file_exists($path_file_updater)){
-    echo 'Updating web site from Origin ...', PHP_EOL;
-
-    $cmd  = 'TERM=xterm && ';
-    $cmd .= 'cd ' . $path_dir_root . DIR_SEP . ' && ';
-    $cmd .= 'pwd &&';
-    $cmd .= "php {$name_file_updater} 2>&1";
-
-    echo 'PathDirExe: ', $path_file_updater, PHP_EOL;
-    echo 'CMD: ', $cmd, PHP_EOL, PHP_EOL;
-
-    echo `$cmd`, PHP_EOL, PHP_EOL;
-
-    @ob_flush();
-    @flush();
-}else{
-    echo 'Not found:', $path_file_updater, PHP_EOL;
+if(empty($path_dir_root)){
+    echo '❌ Error: ';
+    die('Run it via Web server' . PHP_EOL);
 }
+$path_dir_updater  = $path_dir_root . DIR_SEP . $name_dir_parent . DIR_SEP;
+$path_file_updater = $path_dir_updater . DIR_SEP . $name_file_updater;
+$path_file_updater = realpath($path_file_updater);
+
+if(! file_exists($path_file_updater)){
+    dieMsg('Not found:', $path_file_updater);
+}
+
+$path_dir_updater = dirname($path_file_updater);
+
+echo '✅ Updater found.', PHP_EOL;
+echo "\t", 'Updating web site from Origin ...', PHP_EOL;
+
+$cmd  = 'TERM=xterm && ';
+$cmd .= 'cd ' . $path_dir_updater . DIR_SEP . ' && ';
+$cmd .= 'pwd && ';
+$cmd .= "php {$name_file_updater} 2>&1";
+
+echo "\t", '- Path updater  : ', $path_file_updater, PHP_EOL;
+echo "\t", '- Command to run: ', $cmd, PHP_EOL, PHP_EOL;
+
+echo `$cmd`, PHP_EOL, PHP_EOL;
+
+@ob_flush();
+@flush();
+
 
 /* ============================================================== [Functions] */
 
-
+function dieMsg($string)
+{
+    echo $string, PHP_EOL;
+    die;
+}
 
 
 
